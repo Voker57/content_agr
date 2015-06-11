9c154b305553291858e8f50a52b4c36e923aa583
Vitalik <vitalikstetsenko@hotmail.com>
2015-06-11 19:42:56 +0300

    added all category

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,17 +1,22 @@
 class CategoryController < ApplicationController
   def world
+    @world_articles = Article.where( world: true )
   end
 
   def russian
+    @russian_articles = Article.where( russian: true )
   end
 
   def social
+    @social_articles = Article.where( social: true )
   end
 
   def political
+    @political_articles = Article.where( political: true )
   end
 
   def business
+    @business_articles = Article.where( business: true )
   end
 
   def science
@@ -19,5 +24,6 @@ class CategoryController < ApplicationController
   end
 
   def sport
+    @sport_articles = Article.where( sport: true )
   end
 end
