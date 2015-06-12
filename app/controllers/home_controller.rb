8bb439b375c86c1da7d39626c77a9ee37f1b7df3
Vitalik <vitalikstetsenko@hotmail.com>
2015-06-12 16:24:27 +0300

    deploy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @hot_articles = Article.where( hot: true )
+    @top_speak_articles = Article.where( top_read: true )
+    @russian_articles = Article.where( russian: true )
   end
 
 end
