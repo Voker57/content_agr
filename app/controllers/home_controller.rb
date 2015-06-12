d916576534b3a1ca4f48410644d9c2afe94b24b1
Vitalik <vitalikstetsenko@hotmail.com>
2015-06-12 17:10:06 +0300

    add content l_sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @hot_articles = Article.where( hot: true )
-    @top_speak_articles = Article.where( top_read: true )
+    @top_speak_articles = Article.where( top_speak: true )
     @russian_articles = Article.where( russian: true )
+    @top_read_articles = Article.where( top_read: true )
   end
 
 end
