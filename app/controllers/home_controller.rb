2677f3242b3addfaeff36f1875c87c529bb7f0b9
Vitalik <vitalikstetsenko@hotmail.com>
2015-06-14 01:47:03 +0300

    index 50%

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    @category = Category.main
-    @hot_articles = Category.most_visited.articles
-    @most_discussed_articles = Category.most_discussed.articles
-    @russian_articles = Category.russia.articles
-    @most_readest_articles = Category.most_readest.articles.limit( 2 )
+
+    @category = Category.main.first
+    @hot_articles = Category.most_visited.first.articles
+    @most_discussed_articles = Category.most_discussed.first.articles
+    @russian_articles = Category.russia.first.articles
+    @most_readest_articles = Category.most_readest.first.articles.limit( 2 )
     @articles = @category.articles
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,10 +3,10 @@ class Category < ActiveRecord::Base
   has_many :category_articles
   has_many :articles, through: :category_articles
 
-  scope :most_visited,   -> { where( title: 'most_visited' ).first }
-  scope :most_readest,   -> { where( title: 'most_visited' ).first }
-  scope :most_discussed, -> { where( title: 'most_discussed' ).first }
-  scope :russia,         -> { where( title: 'russia' ).first }
-  scope :main,           -> { where( title: 'main' ).first }
+  scope :most_visited,   -> { where( title: 'most_visited' ) }
+  scope :most_readest,   -> { where( title: 'most_visited' ) }
+  scope :most_discussed, -> { where( title: 'most_discussed' ) }
+  scope :russia,         -> { where( title: 'russia' ) }
+  scope :main,           -> { where( title: 'main' ) }
 
 end
