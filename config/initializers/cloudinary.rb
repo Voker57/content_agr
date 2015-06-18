945021c05a942b242603626419effac65e541b3f
Vitalik <vitalikstetsenko@hotmail.com>
2015-06-18 17:23:59 +0300

    add cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do | config |
+  config.cloud_name  = 'hjy4f5gxk'
+  config.api_key = '915511988111278'
+  config.api_secret = 'LcZ6w1WmtbQSnF6mXJHjwBzsmY4'
+  config.cdn_subdomain = true
+end
