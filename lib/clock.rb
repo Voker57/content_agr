be2fc1c9a3ce1f58eb9874f34d3c146fe1c2c38c
Vitalik <vitalikstetsenko@hotmail.com>
2015-07-06 16:17:07 +0300

    scheduled

---
+++ lib/clock.rb
@@ -0,0 +1,9 @@
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
+require 'clockwork'
+
+include Clockwork
+
+every( 10.second, 'Start Mechanize agent' )                 { Article.agent_session }
+every( 10.second, 'Sort according Like to the VKontakte' )  { Sort.puts }
+every( 10.second, 'Published Articles' )                    { Publish.publication }
