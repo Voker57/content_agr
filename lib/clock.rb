5fae880112006741e6f8fa918200408c32b22244
Vitalik <vitalikstetsenko@hotmail.com>
2015-07-06 16:25:57 +0300

    little fix

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,6 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 
 include Clockwork
+include Publish
+include Sort
 
 every( 10.second, 'Start Mechanize agent' )                 { Article.agent_session }
 every( 10.second, 'Sort according Like to the VKontakte' )  { Sort.puts }
