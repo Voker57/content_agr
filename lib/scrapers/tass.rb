84c53b1700136680d24fe480fc57007e29711934
Voker57 <voker57@gmail.com>
2015-07-11 18:55:35 +0300

    TASS & vm scrapers

---
+++ lib/scrapers/tass.rb
@@ -0,0 +1,15 @@
+module Scrapers::Tass
+	def self.get_article_links
+		links = []
+		site_url = "http://tass.ru/"
+		www = ScrapeUtils.new_mechanize
+		newslist = JSON.parse(www.get("http://tass.ru/api/news/lenta?limit=50&sections%5B%5D=23&sections%5B%5D=22&sections%5B%5D=4730&sections%5B%5D=25&sections%5B%5D=24&sections%5B%5D=27&sections%5B%5D=2176&sections%5B%5D=28&sections%5B%5D=4630&sections%5B%5D=4614").body)
+		newslist["articles"].map{|a| a["url"]}
+	end
+	
+	def self.get_article(link)
+		www = ScrapeUtils.new_mechanize
+		article_page = www.get link
+		return({title: article_page.at("h1[@class='b-material__title']").inner_text.strip, text: article_page.search("div[@class='b-material-text__l']/p").to_s})
+	end
+end
---
+++ lib/scrapers/vm.rb
@@ -0,0 +1,18 @@
+module Scrapers::Vm
+	def self.get_article_links
+		links = []
+		site_url = "http://vm.ru/news"
+		www = ScrapeUtils.new_mechanize
+		newslist = www.get "http://vm.ru/news"
+		newslist.search("div[@id='newslist']//ul[@class='articles-list']/li").each do |li|
+			links << URI.parse("http://vm.ru/").merge(li.at("a")["href"]).to_s
+		end
+		links
+	end
+	
+	def self.get_article(link)
+		www = ScrapeUtils.new_mechanize
+		article_page = www.get link
+		return({title: article_page.at("article[@class='article 111']//h1").inner_text.strip, text: article_page.at("article[@class='article 111']//div[@class='content clearfix']").inner_html})
+	end
+end
