3b3169bed62fc61a01616a21ec41f7db38f2bf9f
Voker57 <voker57@gmail.com>
2015-07-13 00:42:54 +0300

    mailru scraper

---
+++ lib/scrapers/mailru.rb
@@ -0,0 +1,19 @@
+module Scrapers::Mailru
+	def self.get_article_links
+		links = []
+		site_url = "https://news.mail.ru/"
+		www = ScrapeUtils.new_mechanize
+		newslist = www.get site_url
+		newslist.search("div[@class='c-notified js-topnews']/section/div/div").each do |par|
+			links << URI.parse(site_url).merge(par.at("a")["href"]).to_s
+		end
+		links
+	end
+	
+	def self.get_article(link)
+		www = ScrapeUtils.new_mechanize
+		article_page = www.get link
+		content = article_page.search("div[@class='js-newstext s-text']/index/p").to_s
+		return({title: article_page.at("h1/index").inner_text.strip, text: content})
+	end
+end
