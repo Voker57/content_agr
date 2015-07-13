7c445ab9994342788703836b0d85624a1ad38149
Voker57 <voker57@gmail.com>
2015-07-13 13:07:52 +0300

    rg scraper fix

--- lib/scrapers/rg.rb
+++ lib/scrapers/rg.rb
@@ -4,7 +4,7 @@ module Scrapers::Rg
 		site_url = "http://www.rg.ru/news.html"
 		www = ScrapeUtils.new_mechanize
 		newslist = www.get site_url
-		newslist.search("div[@class='zg']").each do |par|
+		(newslist.search("div[@class='content4-1l']/div[@class='zg']") + newslist.search("div[@class='content4-2l']/div[@class='zg']") + newslist.search("div[@class='content4-3l']/div[@class='zg']") + newslist.search("div[@class='content4-4l']/div[@class='zg']")).each do |par|
 			links << URI.parse(site_url).merge(par.at("a")["href"]).to_s
 		end
 		links
@@ -16,6 +16,8 @@ module Scrapers::Rg
 		if article_page.at("div[@class='s-large-title']")
 			content = article_page.search("div[@class='s-large-announce-text']/p").to_s
 			return({title: article_page.at("div[@class='s-large-title']").inner_text.strip, text: content})
+		elsif article_page.at("div[@class='content-box-item__info__head__title']")
+			return({title: article_page.at("div[@class='content-box-item__info__head__title']").inner_text.strip, text: article_page.search("div[@class='content-box-item__info__uannounce']/p").to_s})
 		else
 			content = article_page.search("div[@class='main-text']/p").to_s
 			return({title: article_page.at("h1").inner_text.strip, text: content})
